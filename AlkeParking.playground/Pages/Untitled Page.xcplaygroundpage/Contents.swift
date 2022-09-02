0b32e998ad942e80bbb0d13728e7369387e7bd3c
Ana Lucia Blanco <analuciablanco@outlook.com>
2022-09-01 22:25:19 -0500

    propiedades establecidas, protocolo conformado

--- AlkeParking.playground/Pages/Untitled Page.xcplaygroundpage/Contents.swift
+++ AlkeParking.playground/Pages/Untitled Page.xcplaygroundpage/Contents.swift
@@ -1,1 +1,31 @@
 import Foundation
+
+protocol Parkable {
+    var plate: String { get }
+    // var vehicleType: vehicleType { get }
+    var date: Date { get }
+    var hasDiscountCard: Bool { get set }
+    var totalParkedTime: Double { get }
+}
+
+struct Parking {
+    var vehicles: Set<Vehicle> = []
+}
+
+struct Vehicle: Parkable, Hashable {
+    var totalParkedTime: Double
+    
+    var date: Date
+    
+    var hasDiscountCard: Bool
+    
+    let plate: String
+    
+    func hash(into hasher: inout Hasher) {
+        hasher.combine(plate)
+    }
+    
+    static func ==(lhs: Vehicle, rhs: Vehicle) -> Bool {
+        return lhs.plate == rhs.plate
+    }
+}

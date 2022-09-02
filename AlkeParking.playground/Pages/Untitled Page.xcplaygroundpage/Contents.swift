3bef9153c939db6ece2e024166acb353dde7f134
Ana Lucia Blanco <analuciablanco@outlook.com>
2022-09-01 22:53:32 -0500

    creacion de instancias

--- AlkeParking.playground/Pages/Untitled Page.xcplaygroundpage/Contents.swift
+++ AlkeParking.playground/Pages/Untitled Page.xcplaygroundpage/Contents.swift
@@ -1,5 +1,6 @@
 import Foundation
 
+// MARK: - Protocols
 protocol Parkable {
     var plate: String { get }
     var type: VehicleType { get }
@@ -9,6 +10,7 @@ protocol Parkable {
     var fee: Int { get }
 }
 
+// MARK: - Enums
 enum VehicleType {
     case car
     case moto
@@ -16,14 +18,20 @@ enum VehicleType {
     case bus
 }
 
+// MARK: - Structs
 struct Parking {
     var vehicles: Set<Vehicle> = []
+    
+    /* La función es definida como mutating pues al estar modificando una propiedad de una struct debe indicarse explícitamente que se desea que la instancia sea modificada. */
+    mutating func checkInVehicle(_ vehicle: Vehicle, onFinish: (Bool) -> Void) {
+        
+    }
 }
 
 struct Vehicle: Parkable, Hashable {
     let plate: String
     let type: VehicleType
-    let checkInTime: Date = Date()
+    let checkInTime: Date
     
     var parkedTime: Int {
         return Calendar.current.dateComponents([.minute], from: checkInTime, to: Date()).minute ?? 0
@@ -48,3 +56,23 @@ struct Vehicle: Parkable, Hashable {
         return lhs.plate == rhs.plate
     }
 }
+
+// MARK: - Instances
+var alkeParking = Parking()
+
+let car = Vehicle(plate: "AA111AA", type: VehicleType.car, checkInTime: Date(), discountCard: "DISCOUNT_CARD_001")
+let car2 = Vehicle(plate: "AA111AA", type: VehicleType.car,
+checkInTime: Date(), discountCard: "DISCOUNT_CARD_003")
+let moto = Vehicle(plate: "B222BBB", type: VehicleType.moto, checkInTime: Date(), discountCard: nil)
+let miniBus = Vehicle(plate: "CC333CC", type: VehicleType.miniBus, checkInTime: Date(), discountCard:
+
+nil)
+let bus = Vehicle(plate: "DD444DD", type: VehicleType.bus, checkInTime: Date(), discountCard: "DISCOUNT_CARD_002")
+
+alkeParking.vehicles.insert(car)
+alkeParking.vehicles.insert(car2)
+alkeParking.vehicles.insert(moto)
+alkeParking.vehicles.insert(miniBus)
+alkeParking.vehicles.insert(bus)
+
+alkeParking.vehicles.remove(moto)
